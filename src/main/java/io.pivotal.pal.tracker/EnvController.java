2eafccfeb0b606ad274c7b9fbcd54c9102a03d67
Vijay Nambiar <vnambiar@travelers.com>
2018-06-12 14:37:24 -0600

    WE DID IT!

--- src/main/java/io.pivotal.pal.tracker/EnvController.java
+++ src/main/java/io.pivotal.pal.tracker/EnvController.java
@@ -1,4 +1,34 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class EnvController {
+    private String port;
+    private String mem_lim;
+    private String cf_inst_ind;
+    private String cf_inst_addr;
+    private Map<String,String> env = new HashMap<String,String>();
+
+    public EnvController(@Value("${PORT:NOT SET}") String port, @Value("${MEMORY_LIMIT:NOT SET}") String memory_limit, @Value("${CF_INSTANCE_INDEX:NOT SET}") String cf_inst_index, @Value("${CF_INSTANCE_ADDR:NOT SET}") String cf_inst_addr)
+    {
+        this.port = port;
+        this.mem_lim = memory_limit;
+        this.cf_inst_ind = cf_inst_index;
+        this.cf_inst_addr = cf_inst_addr;
+        env.put("PORT",this.port);
+        env.put("MEMORY_LIMIT",this.mem_lim);
+        env.put("CF_INSTANCE_INDEX",this.cf_inst_ind);
+        env.put("CF_INSTANCE_ADDR",this.cf_inst_addr);
+    }
+
+    @GetMapping("/env")
+    public Map<String,String> getEnv() {
+        return this.env;
+    }
 }
---
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -0,0 +1,24 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WelcomeController {
+
+    public String hello = "hello";
+    private String message;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String msg)
+    {
+        this.message = msg;
+    }
+
+    @GetMapping("/")
+    public String sayHello()
+    {
+        return message;
+    }
+}
